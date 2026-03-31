7071268ea00cb548b953ee49724d39bd98976334
fiveseveN <nguyentanphat0309st@gmail.com>
2026-03-31 22:49:03 +0700

    Cập nhật src/App.jsx để sử dụng Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,22 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import MainLayout from './components/MainLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Route công khai: Ai cũng xem được */}
         <Route path="/login" element={<Login />} />
 
-        {/* Route bảo vệ: Chỉ Admin/User đã login mới vào được  */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+        {/* Các trang yêu cầu đăng nhập và có Sidebar */}
+        <Route element={<ProtectedRoute><MainLayout /></ProtectedRoute>}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/products" element={<div>Trang Sản phẩm</div>} />
+          <Route path="/inventory" element={<div>Trang Kho</div>} />
+          {/* Thêm các route khác ở đây */}
+        </Route>
 
-        {/* Điều hướng mặc định: Nếu vào link lạ, đẩy về Dashboard hoặc Login */}
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
